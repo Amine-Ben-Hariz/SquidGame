4e5357a6a63ec2faf6656641156c6bcf841c6cc6
Amine Ben Hariz <benharizamine4@gmail.com>
2026-03-23 06:14:38 +0100

    updated

--- ec/ec/settings.py
+++ ec/ec/settings.py
@@ -134,8 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = ""
+EMAIL_PORT = ""
 EMAIL_USE_TLS = True
 
 # TODO: Replace these with your actual Gmail address and an App Password.
